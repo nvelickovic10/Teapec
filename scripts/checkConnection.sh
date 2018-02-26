eb36538b057ff6cb387d4e842cfab7d6a9cec333
Nemanja Velickovic <nemanja.velickovic@livenation.com>
2018-02-26 15:41:49 +0100

    change

--- scripts/checkConnection.sh
+++ scripts/checkConnection.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo $PUBLIC_IP_ADDRESS
-
 CURRENT_IP_ADDRESS=$(curl ipinfo.io/ip)
 
 echo $PUBLIC_IP_ADDRESS $CURRENT_IP_ADDRESS
+if [[ ${CURRENT_IP_ADDRESS} == ${PUBLIC_IP_ADDRESS} ]]; then
+    echo "SAME"
+else
+    echo "DIFFERENT"
+    export PUBLIC_IP_ADDRESS=${CURRENT_IP_ADDRESS}
+fi
+#END
