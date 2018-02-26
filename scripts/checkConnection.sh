ec790a55cb168acedd3eadd5ff6f18b622c649e3
Nemanja Velickovic <nemanja.velickovic@livenation.com>
2018-02-26 16:09:28 +0100

    aa

--- scripts/checkConnection.sh
+++ scripts/checkConnection.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-CURRENT_IP_ADDRESS=$(curl ipinfo.io/ip)
+cd "$(dirname "$0")"
+
+PUBLIC_IP_ADDRESS_FILE=./PUBLIC_IP_ADDRESS
+
+CURRENT_IP_ADDRESS=$(curl -s ipinfo.io/ip)
+PUBLIC_IP_ADDRESS=$(cat ${PUBLIC_IP_ADDRESS_FILE})
+
+function logInfo {
+    echo "INFO: $1"
+}
 
 echo $PUBLIC_IP_ADDRESS $CURRENT_IP_ADDRESS
 if [[ ${CURRENT_IP_ADDRESS} == ${PUBLIC_IP_ADDRESS} ]]; then
-    echo "SAME"
+    logInfo "IP address did not change"
 else
-    echo "DIFFERENT"
-    PUBLIC_IP_ADDRESS=${CURRENT_IP_ADDRESS}
+    logInfo "IP address changed!!"
+    echo ${CURRENT_IP_ADDRESS} >> ${PUBLIC_IP_ADDRESS_FILE}
 fi
 #END
