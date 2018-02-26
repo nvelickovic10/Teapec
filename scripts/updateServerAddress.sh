062b9dec99f5eaca988fce552400511bbe062a1b
Nemanja Velickovic <nemanja.velickovic@livenation.com>
2018-02-26 17:44:14 +0100

    dsds

--- scripts/updateServerAddress.sh
+++ scripts/updateServerAddress.sh
@@ -6,6 +6,11 @@ cd "$(dirname "$0")"
 # Include common functions
 . common.sh
 
+if [[ ${UPDATE_SERVER_ADDRESS} == false ]]; then
+    logInfo "SKIPPING updateServerAddress.sh"
+    exit 0
+fi
+
 logInfo "STARTED updateServerAddress.sh"
 
 PUBLIC_IP_ADDRESS_FILE=./PUBLIC_IP_ADDRESS
